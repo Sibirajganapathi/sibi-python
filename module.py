783147d6fa7f49dd81f7a8fe5deb5122f052c4e7
Sibirajganapathi <157332623+Sibirajganapathi@users.noreply.github.com>
2024-02-14 07:10:51 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,11 @@
+import arithmetic
+print("add value",arithmetic.add(10,30))
+#second method
+from arithmetic import *
+print(sub(45,20))
+print(multiple(135,8))
+print(modulus(4,9))
+print(division(234,7))
+
+from package.permiter import *
+print(circle(45))
